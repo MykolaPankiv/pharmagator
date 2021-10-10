e885990b376e722f3242a416abf77fa087eefd50
Anastasia Bondarenko <ne7sgreen@gmail.com>
2021-10-10 22:46:51 +0300

    add lection code

---
+++ api/src/main/java/com/eleks/academy/pharmagator/entities/Pharmacy.java
@@ -0,0 +1,20 @@
+package com.eleks.academy.pharmagator.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "pharmacies")
+public class Pharmacy {
+    @Id
+    private long id;
+    private String name;
+    private String medicineLinkTemplate;
+}
