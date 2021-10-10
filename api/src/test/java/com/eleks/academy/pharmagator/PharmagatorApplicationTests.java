227ac3aee0cd5c81c70127e1b5d6522a1f6864b6
Anastasia Bondarenko <ne7sgreen@gmail.com>
2021-10-10 15:32:20 +0300

    change structure a bit

--- api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
@@ -1,7 +1,8 @@
-package com.example.pharmagator;
+package com.eleks.academy.pharmagator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class PharmagatorApplicationTests {
